bf2829a5d34b1d78108d7fde75cee1b65870641a
x1as <umarov.s.i.1.19@gmail.com>
2022-05-11 19:03:32 +0300

    has added colored depth

--- src/components/UsersTable/UsersTableUserRow.vue
+++ src/components/UsersTable/UsersTableUserRow.vue
@@ -5,7 +5,7 @@
         <v-icon v-if="depth > 0" light>mdi-subdirectory-arrow-right</v-icon>
         <span class="username">{{ user.name }}</span>
       </td>
-      <td>{{ user.phone }}</td>
+      <td :style="indentColor">{{ user.phone }}</td>
     </tr>
     <UsersTableUserRow
       v-for="user in childs"
@@ -28,7 +28,15 @@ export default {
   props: ["user", "childs", "depth"],
   computed: {
     indent() {
-      return { "padding-left": `${this.depth * 20 + 16}px` };
+      return {
+        "padding-left": `${this.depth * 20 + 16}px`,
+        "background-color": `hsl(320, 0%, ${100 - ((this.depth * 5) % 100)}%)`,
+      };
+    },
+    indentColor() {
+      return {
+        "background-color": `hsl(320, 0%, ${100 - ((this.depth * 5) % 100)}%)`,
+      };
     },
   },
 };
@@ -41,5 +49,6 @@ export default {
   overflow: hidden;
   -o-text-overflow: ellipsis;
   text-overflow: ellipsis;
+  color: hsl(hue, saturation, lightness);
 }
 </style>
